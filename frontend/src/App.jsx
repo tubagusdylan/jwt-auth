54a0f110cd6f0ee84fc109a3454d020445c2e4e2
tubagusdylan <tubagus.dylanr@gmail.com>
2023-08-11 19:08:43 +0700

    add register form and router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 
 function App() {
   return (
     <div className="container bg-purple-950 min-h-screen">
-      <Login />
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
 
-export const Login = () => {
+export const Register = () => {
   const [isShowPassword, setIsShowPassword] = useState(false);
 
   const handleShowPassword = () => {
@@ -10,14 +10,20 @@ export const Login = () => {
   };
 
   return (
-    <section className="py-[20vh]">
+    <section className="py-[10vh]">
       <div className="w-[400px] mx-auto p-4  bg-white rounded-lg">
-        <h1 className="text-center font-bold text-3xl pt-1 pb-3 text-purple-400">Login</h1>
+        <h1 className="text-center font-bold text-3xl pt-1 pb-3 text-purple-400">Register</h1>
         <form>
+          <div>
+            <label htmlFor="name" className="text-purple-800 font-semibold text-lg">
+              Name
+              <input type="text" name="name" id="name" placeholder="Enter your name" className="block w-full border border-purple-500 p-3 rounded-lg my-2 outline-none focus:ring focus:ring-blue-200" autoFocus />
+            </label>
+          </div>
           <div>
             <label htmlFor="email" className="text-purple-800 font-semibold text-lg">
               Email
-              <input type="email" name="email" id="email" placeholder="Enter your email" className="block w-full border border-purple-500 p-3 rounded-lg my-2 outline-none focus:ring focus:ring-blue-200" autoFocus />
+              <input type="email" name="email" id="email" placeholder="Enter your email" className="block w-full border border-purple-500 p-3 rounded-lg my-2 outline-none focus:ring focus:ring-blue-200" />
             </label>
           </div>
           <div className="relative">
@@ -31,13 +37,19 @@ export const Login = () => {
               <HiOutlineEyeOff size={25} className="absolute top-12 mt-[3px] right-3 cursor-pointer text-purple-400" onClick={handleShowPassword} />
             )}
           </div>
-          <button className="w-full bg-purple-500 p-3 rounded-lg mt-5 font-semibold text-white text-lg hover:bg-purple-400 active:bg-purple-800">Login</button>
+          <div className="relative">
+            <label htmlFor="conf_password" className="text-purple-800 font-semibold text-lg">
+              Confirm Password
+              <input type="password" name="conf_password" id="conf_password" placeholder="*******" className="block w-full border border-purple-500 p-3 rounded-lg my-2 outline-none focus:ring focus:ring-blue-200" />
+            </label>
+          </div>
+          <button className="w-full bg-purple-500 p-3 rounded-lg mt-5 font-semibold text-white text-lg hover:bg-purple-400 active:bg-purple-800">Register</button>
         </form>
         <div className="w-full mt-5 text-center">
           <span className="t">
-            Do not have account?{" "}
-            <Link to="/register" className="text-purple-500 hover:text-purple-800 hover:underline">
-              Register now
+            Already have account?{" "}
+            <Link to="/" className="text-purple-500 hover:text-purple-800 hover:underline">
+              Login now
             </Link>
           </span>
         </div>
